aca220788dbb1069593acec23c5095d394415666
Numan506 <78308167+Numan506@users.noreply.github.com>
2021-05-02 20:25:39 +0600

    Add files via upload

---
+++ Area of shape.py
@@ -0,0 +1,18 @@
+
+base = float(input("please give the Base number : "))
+height = float(input("please give the height number : "))
+width = float(input("please give the Width number : "))
+radisu = float(input("please give the Radius number : "))
+square = float(input("please give the Square number : "))
+
+print("..........Triangle.........\n")
+print("The Result Triangle is: 0.5*base*height = "f"{base} *{height} = {0.5*base*height}\n")
+
+print("..........Rectangle.........\n")
+print("The Result Rectangle is : height*width = "f"{height} *{width} = {height*width}\n")
+
+print("..........Circle.........\n")
+print("The Result Circle is : PI*radisu*radisu = "f"{radisu} *{radisu} = {3.1416*radisu*radisu}\n")
+
+print("..........Square.........\n")
+print("The Result square is : square*square  = "f"{square} *{square} = {square*square}\n")
---
+++ Math Related Function.py
@@ -0,0 +1,19 @@
+
+from math import *
+num1 = float(input("Please given the num1: "))
+num2 = float(input("Please given the num2: "))
+abs_number= float(input("Please given the abs_number: "))
+sqrt_number= float(input("Please given the sqrt_number: "))
+floor_number= float(input("Please given the floor_number: "))
+ceil_number= float(input("Please given the ceil_number: "))
+print()
+
+print("......Now print Math related function.......\n ")
+
+print(f"Input {num1} & {num2}" " So Max number is = ",max(num1,num2))
+print(f"Input {num1} & {num2}" " So Min number is = ",min(num1,num2))
+print(f"Input {num1} & {num2}" " So Pow number is = ",pow(num1,num2))
+print(f"Input {abs_number}" " So abs_number is = ",abs(abs_number))
+print(f"Input {sqrt_number}" " So sqrt_number is = ",sqrt(sqrt_number))
+print(f"Input {floor_number}" " So floor_number is = ",floor(floor_number))
+print(f"Input {ceil_number}" " So ceil_number is = ",ceil(ceil_number))
