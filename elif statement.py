d411fe0e8f53f7b8a6d45c6c8a2b439ac5ea8e39
Numan506 <78308167+Numan506@users.noreply.github.com>
2021-05-03 23:21:53 +0600

    Add files via upload

---
+++ elif statement.py
@@ -0,0 +1,17 @@
+
+marks = float(input("please given the number: "))
+
+if(marks>=80):
+    print("Result is: A+")
+elif (marks >= 70):
+    print("Result is: A")
+elif (marks >= 60):
+    print("Result is: A-")
+elif (marks >= 50):
+    print("Result is: B")
+elif (marks >= 40):
+    print("Result is: C")
+elif (marks >= 33):
+    print("Result is: D")
+else:
+    print("Result is fail")
