3bc7d16bd48dc4bc8b136e8ee51e310a132d65e7
Numan506 <78308167+Numan506@users.noreply.github.com>
2021-05-04 22:43:21 +0600

    Add files via upload

---
+++ Odd & Even using While Loop.py
@@ -0,0 +1,17 @@
+
+num = int(input("enter the gven number: "))
+
+i =2;
+while(i<=num):
+    print("even number is: ",i)
+    i = i+2
+print("The program is end ")
+
+print()
+num = int(input("enter the gven number: "))
+
+i =1;
+while(i<=num):
+    print("odd number is: ",i)
+    i = i+2;
+print("the program is end")
---
+++ Sum of n numbers.py
@@ -0,0 +1,10 @@
+
+num = int(input("please given the number: "))
+
+i = 0
+sum = 0
+
+while(i<=num):
+    sum = sum + i
+    i = i+1
+print("Total sum is: ",sum)
