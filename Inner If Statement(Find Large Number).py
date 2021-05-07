bbca529fff010b2221766068ad0656dbab286225
Numan506 <78308167+Numan506@users.noreply.github.com>
2021-05-07 23:58:24 +0600

    Add files via upload

---
+++ Inner If Statement(Find Large Number).py
@@ -0,0 +1,21 @@
+
+
+num1 = int(input("Please given the first number: "))
+num2 = int(input("Please given the second number: "))
+num3 = int(input("Please given the third number: "))
+
+if(num1>num2):
+    if(num1>num3):
+        print("large number is: ",num1)
+    else:
+        print("large number is: "+num3)
+
+
+else:
+    (num2>num1)
+    if(num2>num3):
+        print("large number is: " ,num2)
+    else:
+        print("large number is: ",num3 )
+
+print("the program is end")
