930d95729a28fa6309f07b8ce7b4eb538f6a3259
Numan506 <78308167+Numan506@users.noreply.github.com>
2021-05-17 14:34:13 +0600

    Add files via upload

---
+++ Dictionaries Program.py
@@ -0,0 +1,16 @@
+
+
+studentId ={
+    506 : "Numan",
+    507 : "jakaria",
+    508 : " Rakib",
+    509 : "Khalek",
+    510 : "Mahiubur",
+    511: "Khalek",
+    512: "faysal",
+    513: "nuhin",
+    514: "abdul",
+
+}
+print(studentId.get(509,"not a valid id"))
+
